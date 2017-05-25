652e3e7daef9b3abd89e3ef7fbfe8f1c0b9d4fb5
briank621 <bck2116@columbia.edu>
2017-05-25 12:59:49 -0400

    Added comments and block enumeration functionality

--- ipblock.java
+++ ipblock.java
@@ -1,5 +1,17 @@
+/**
+	*
+	*The ipblock class provides both testing for membership within a subnet
+	*as well as enumerating all ip addresses within a subnet
+	*
+	*@author	Brian Kim
+*/
 public class ipblock{
 	
+	/*
+		The convert function converts a IPv4 String into the corresponding
+		long representation.
+		-Throws an Illegal Argument Exception if the input is malformed
+	*/
 	public static long convert(String s) throws IllegalArgumentException{
 		String[] blocks = s.split("\\.");
 		if(blocks.length != 4)
@@ -21,13 +33,33 @@ public class ipblock{
 		return output;
 	}
 
+	/*
+	printAddress takes in a long integer and prints out the
+	corresponding IPv4 address
+	*/
+	public static void printAddress(long addr){
+		long bitmask = (1L << 8) - 1;
+		StringBuilder sb = new StringBuilder();
+		//checks in blocks of 8 bits
+		for(int i = 0; i < 4; i++){
+			long part = addr & bitmask;
+			sb.insert(0,part);
+			sb.insert(0, ".");
+			addr >>= 8;
+		}
+		//can be optimized by storing everything in a buffer
+		//and then printing at the end
+		System.out.println(sb.substring(1));
+	}
+
 	public static void main(String[] args){
-		if(args.length < 2){
+		if(args.length < 1){
 			System.out.println("Not enough arguments.");
 			System.out.println("Usage: ipblock BLOCK ADDR");
 			return;
 		}	
 
+		//parse the subnet
 		String subnet = args[0];
 		String[] ipBlock = subnet.split("/");
 		if(ipBlock.length != 2){
@@ -35,6 +67,7 @@ public class ipblock{
 			return;
 		}
 
+		//extracts prefix
 		int prefix = 0;
 		try{
 			prefix = Integer.parseInt(ipBlock[1]);
@@ -48,11 +81,30 @@ public class ipblock{
 			return;
 		}
 
-		String addr = args[1];
-		long block = 0L, check = 0L;
-
+		//extracts the subnet starting address/block
+		long block = 0L;
 		try{
 			block = convert(ipBlock[0]);
+		}
+		catch(IllegalArgumentException e){
+			System.out.println("Error parsing addresses: " + e.getMessage());
+			return;
+		}
+		
+		//printing the address
+		if(args.length == 1){
+			long bitmask = (1L << prefix) - 1;
+			bitmask <<= (32 - prefix);
+			bitmask &= block; //extracts the significant bits
+			for(int i = 0; i < (1L << (32 - prefix)); i++)
+				printAddress(bitmask + i);
+			return;
+		}
+
+		//verification with a second argument
+		String addr = args[1];
+		long check = 0L;
+		try{
 			check = convert(addr);
 		}
 		catch(IllegalArgumentException e){
@@ -60,13 +112,10 @@ public class ipblock{
 			return;
 		}
 
-		// System.out.printf("%32s\n", Long.toString(block, 2));
-		// System.out.printf("%32s\n", Long.toString(check, 2));
-
+		//extract the significant bits 
+		//the XOR of the bitmask and address should be zero
 		long bitmask = (1L << prefix) - 1;
 		bitmask <<= (32 - prefix);
-		// System.out.printf("%32s\n", Long.toString(bitmask, 2));
-
 		bitmask &= block;
 		bitmask ^= check;
 		bitmask >>= (32 - prefix);
